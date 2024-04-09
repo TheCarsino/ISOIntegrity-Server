fef0f52b87d4d2ca6f881817bd0b49392958e190
Lucas Podestá <carsino12@gmail.com>
2024-04-08 21:57:49 -0500

    Server commit for deploy v.1.3
    Test for DB Connection

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 export const SV_PORT = 3000;
-export const DB_HOST = "localhost";
+export const DB_HOST = "127.0.0.1";
 export const DB_PORT = 3306;
 export const DB_USER = "root";
-export const DB_CREDENTIALS = "faztpassword";
-export const DB_NAME = "tasksdb";
+export const DB_CREDENTIALS = "122www";
+export const DB_NAME = "isointegrity-dev";
--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import cors from "cors";
+import { pool } from "./db.js";
 import { SV_PORT } from "./config.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//Ping test
+app.get("/ping", async (req, res) => {
+  const [result] = await pool.query('SELECT "Pong" AS Result');
+  res.json(result[0]);
+});
+
+/* Routes inclusion */
 
 app.listen(SV_PORT);
 console.log(`Server is listening on port ${SV_PORT}`);
