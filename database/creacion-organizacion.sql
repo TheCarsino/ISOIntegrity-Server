592ab8f5d5dbc3c802d162f3f19f949728c17622
Lucas Podestá <carsino12@gmail.com>
2024-04-24 23:54:37 -0500

    First iteration v1.3
    Update for Querys

--- database/creacion-organizacion.sql
+++ database/creacion-organizacion.sql
@@ -20,10 +20,8 @@ VALUES ('Administrador', 'Acceso total a la información del sistema.', CURDATE(
        ('Colaborador', 'Usuario común del sistema para la generación de inquietudes en el sistema.', CURDATE(), CURDATE(), TRUE);
 
 INSERT INTO User (usuario, contrasena, nombres, apellidos, correo, fecha_creacion, ultima_modificacion, activo)
-VALUES ('admin', '122www', 'System', 'Administrator', 'admin@example.com', CURDATE(), CURDATE(), TRUE);
-
-INSERT INTO User_x_Role_x_Unit (user_id, role_id, unit_area_id, fecha_creacion, ultima_modificacion, activo)
-VALUES (1, 1, NULL, CURDATE(), CURDATE(), TRUE);
+VALUES ('adminTest', '122www', 'System', 'Administrator', 'admintest@example.com', CURDATE(), CURDATE(), TRUE);
+-- Dont forget to update the user later via API
 
 /*
 SAMPLE DATA --> DON'T RUN THIS PART OF THE QUERY --> DO IT FROM THE API
@@ -34,18 +32,18 @@ VALUES ('altadirectiva1', 'altadirectiva', 'John', 'Doe Roe', 'johndoe@example.c
        ('colaborador1', 'colaborador', 'Will', 'Smith Table', 'willsmith@example.com', CURDATE(), CURDATE(), TRUE);
 */       
 
-SELECT * FROM organization;
-SELECT * FROM risktreatment;
+SELECT * FROM Organization;
+SELECT * FROM RiskTreatment;
 
 -- Auth Query
 SELECT 	uru.id, uru.user_id, u.usuario, u.nombres, u.apellidos, u.correo, 
 		uru.role_id, r.nombre AS role_nombre, r.descripcion AS role_descripcion, 
         uru.unit_area_id, COALESCE(a.codigo, 'NULL') AS unit_area_codigo, COALESCE(a.nombre, 'NULL') AS unit_area_nombre
-FROM user_x_role_x_unit AS uru
-INNER JOIN user AS u ON u.id = uru.user_id
-INNER JOIN role AS r ON r.id = uru.role_id
-LEFT JOIN unitarea AS a ON a.id = uru.unit_area_id
-WHERE uru.user_id  = 4
+FROM User_X_Role_X_Unit AS uru
+INNER JOIN User AS u ON u.id = uru.user_id
+INNER JOIN Role AS r ON r.id = uru.role_id
+LEFT JOIN UnitArea AS a ON a.id = uru.unit_area_id
+WHERE uru.user_id  = 1
 AND u.activo = 1
 AND r.activo = 1
 AND uru.activo = 1;
