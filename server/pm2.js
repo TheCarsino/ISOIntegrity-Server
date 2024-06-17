2e04bbc6468635be75377cbbbed1097ac610794c
Lucas Podestá <78005968+TheCarsino@users.noreply.github.com>
2024-06-17 00:08:06 -0500

    Sixth Iteration v.2.6

---
+++ server/pm2.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "isointegrity-server", // Replace with your application name
+      script: "npm run start", // Replace with your actual start script
+      env: {
+        // Define environment variables here if needed
+      },
+    },
+  ],
+};
