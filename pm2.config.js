33e845be3bb3df2d2acdcd75491d382885786c16
Lucas Podestá <78005968+TheCarsino@users.noreply.github.com>
2024-06-17 00:11:23 -0500

    Sixth Iteration v.2.6

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "isointegrity-server", // Replace with your application name
+      name: "server", // Replace with your application name
       script: "npm run start", // Replace with your actual start script
       env: {
         // Define environment variables here if needed
